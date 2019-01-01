29b14c0ddd30b1d93e17099b6e6a969bbf1513cf
paoloach <paolo.achdjian@skillbill.it>
2019-01-01 20:54:34 +0100

    launch file with parameters

--- src/main/kotlin/it/achdjian/plugin/ros/launch/LaunchEditor.kt
+++ src/main/kotlin/it/achdjian/plugin/ros/launch/LaunchEditor.kt
@@ -3,40 +3,91 @@ package it.achdjian.plugin.ros.launch
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.ui.components.textFieldWithBrowseButton
+import com.intellij.ui.components.JBTextField
+import com.intellij.ui.components.fields.IntegerField
 import com.intellij.ui.layout.panel
 import it.achdjian.plugin.ros.ui.LauncherFileChooser
-import javax.swing.JPanel
+import javax.swing.JCheckBox
 
 
 object LaunchFileChooserDescriptor : FileChooserDescriptor(true, false, false, false, false, false)
 
-class LaunchEditor(private val project: Project) : SettingsEditor<LaunchConfiguration>() {
-    private var  browseButton: TextFieldWithBrowseButton? = null
+class LaunchEditor( project: Project) : SettingsEditor<LaunchConfiguration>() {
+    private val browseButton = LauncherFileChooser("Launch file", project, LaunchFileChooserDescriptor)
+    private val rosMasterAddr = JBTextField("127.0.0.1")
+    private val rosMasterPort = IntegerField("11311", 0, 65535)
+    private val screen = JCheckBox("Force output of all local nodes to screen")
+    private val log = JCheckBox("Force output of all local nodes to log")
+    private val wait = JCheckBox("wait for master to start before launching")
+    private val verbose = JCheckBox("verbose printing")
+    private val loggerLevel = ComboBox<String>()
+
+    init {
+        loggerLevel.addItem("debug")
+        loggerLevel.addItem("info")
+        loggerLevel.addItem("warn")
+        loggerLevel.addItem("error")
+        loggerLevel.addItem("fatal")
+        loggerLevel.selectedItem = "info"
+
+        rosMasterPort.value = 11311
+    }
 
     override fun applyEditorTo(launchConfiguration: LaunchConfiguration) {
-        browseButton?.let {
-            launchConfiguration.path = VirtualFileManager.getInstance().findFileByUrl(it.text)
-        }
+        launchConfiguration.path = VirtualFileManager.getInstance().findFileByUrl("file://${browseButton.text}")
+        launchConfiguration.rosMasterAddr=rosMasterAddr.text
+        launchConfiguration.rosMasterPort=rosMasterPort.value
+        launchConfiguration.screen=screen.isSelected
+        launchConfiguration.log=log.isSelected
+        launchConfiguration.wait=wait.isSelected
+        launchConfiguration.verbose=verbose.isSelected
+        launchConfiguration.logLevel=loggerLevel.selectedItem as String
     }
 
     override fun resetEditorFrom(launchConfiguration: LaunchConfiguration) {
-        browseButton?.let {
-            launchConfiguration.path?.let{path->
-                it.text = path.path
-            }
-
+        launchConfiguration.path?.let { path ->
+            browseButton.text = path.path
         }
+        rosMasterAddr.text = launchConfiguration.rosMasterAddr
+        rosMasterPort.value = launchConfiguration.rosMasterPort
+        screen.isSelected = launchConfiguration.screen
+        log.isSelected = launchConfiguration.log
+        wait.isSelected = launchConfiguration.wait
+        verbose.isSelected = launchConfiguration.verbose
+        loggerLevel.selectedItem = launchConfiguration.logLevel
     }
 
-    override fun createEditor() : JPanel {
-        val p =  panel {
-            row("Launch file") {
-                browseButton = textFieldWithBrowseButton("Launch file", "", project, LaunchFileChooserDescriptor)
+    override fun createEditor() = panel {
+        row("Launch file") {
+            browseButton(grow)
+        }
+        titledRow("ROS MASTER") {
+            row("ROS MASTER address") {
+                rosMasterAddr(grow)
+            }
+            row("ROS MASTER port") {
+                rosMasterPort(grow)
             }
         }
-        return p;
+        titledRow("Options") {
+            row("logger level") {
+                loggerLevel(grow)
+            }
+            row {
+                screen(grow)
+            }
+            row {
+                log(grow)
+            }
+            row {
+                wait(grow)
+            }
+            row {
+                verbose(grow)
+            }
+        }
+
     }
 }
--- src/main/kotlin/it/achdjian/plugin/ros/launch/LaunchLauncher.kt
+++ src/main/kotlin/it/achdjian/plugin/ros/launch/LaunchLauncher.kt
@@ -16,10 +16,26 @@ class LaunchLauncher(val launchConfiguration: LaunchConfiguration, environment: 
             val rosVersion = rosEnvironments.getVersion(environment.project)
             rosVersion?.let {rosVersion->
                 val environmentVariables = getEnvironmentVariables(environment.project, rosVersion.env)
-                val cmdLine = GeneralCommandLine(rosVersion.rosLaunch, launchFile.path)
+                val cmdLine = GeneralCommandLine(rosVersion.rosLaunch)
+                if (launchConfiguration.verbose){
+                    cmdLine.addParameter("-v")
+                }
+                if (launchConfiguration.wait){
+                    cmdLine.addParameter("--wait")
+                }
+                if (launchConfiguration.screen){
+                    cmdLine.addParameter("--screen")
+                }
+                if (launchConfiguration.log){
+                    cmdLine.addParameter("--log")
+                }
+                cmdLine.addParameter("--master-logger-level=${launchConfiguration.logLevel}")
+                cmdLine.addParameter(launchFile.path)
                 cmdLine.setWorkDirectory("/home/paolo/ros/irobot")
                 cmdLine.withEnvironment(environmentVariables)
                 cmdLine.withEnvironment("PYTHONUNBUFFERED", "1")
+                val rosMasterUri="http://${launchConfiguration.rosMasterAddr}:${launchConfiguration.rosMasterPort}"
+                cmdLine.withEnvironment("ROS_MASTER_URI", rosMasterUri)
                 val handler = KillableColoredProcessHandler(cmdLine)
                 handler
             } ?: NopProcessHandler()
